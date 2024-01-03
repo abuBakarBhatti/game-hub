f1be5228faa387a55d2655316842e31638273af1
abuBakarBhatti <abubakar85040@gmail.com>
2024-01-04 00:40:55 +0500

    build the game card

--- src/components/useGames.ts
+++ src/components/useGames.ts
@@ -3,16 +3,18 @@ import apiClient from '../services/api-client'
 import { spring } from 'framer-motion'
 import { CanceledError } from 'axios'
 
-const useGames = () => {
-    interface Game{
-        id: number, 
-        name: string
-    }
+export interface Game{
+  id: number, 
+  name: string,
+  background_image: string
+}
 
-    interface FetchGamesResponse{
-        count: number, 
-        results: Game[]
-    }
+interface FetchGamesResponse{
+  count: number, 
+  results: Game[]
+}
+
+const useGames = () => {
 
     const [games, setGames] =useState<Game[]>([])
     const [error, setError] = useState('')
@@ -24,7 +26,7 @@ const useGames = () => {
         apiClient.get<FetchGamesResponse>("/games", { signal: controller.signal })
          .then(res => setGames(res.data.results))
          .catch(err => {
-          if (err instanceof CanceledError) return
+          if (err instanceof CanceledError) return 
           setError(err.message)})
 
         return () => controller.abort()
