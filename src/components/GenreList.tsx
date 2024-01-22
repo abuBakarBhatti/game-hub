742fb3a8d2dc9fe1d5acf2c63af7849ed9294759
abuBakarBhatti <abubakar85040@gmail.com>
2024-01-22 16:01:16 +0500

    Refactoring fetch games with React-Query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,13 +12,13 @@ const GenreList = ({selectedGenre1, onSelectGenre}: Props) => {
     const {data, isLoading, error} = useGenres()
     const skeletons = [1, 2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-    if (error) return null
+    if (error) return null 
   return (  
     <>
     <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
     <List>
         {isLoading && skeletons.map(skeleton => <GenreSkeleton key={skeleton}></GenreSkeleton>)}
-        {data.map(genre => 
+        {data?.results.map(genre => 
         <ListItem key={genre.id} paddingY="5px" >
           <HStack>
             <Image boxSize="32px" objectFit="cover" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
